2e8f7ba278c094b173dce1423eeb7b6a7c16bbff
Otávio Corrêa <otaviolscorrea@hotmail.com>
2023-05-23 03:59:07 -0300

    4 Commit

---
+++ Lista de Exercícios 2 - Recursividade/Ex13.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int verStr(char str[], char subStr[])
+{
+    if (strlen(subStr) > strlen(str))
+    {
+        return 0;
+    }
+    if (strncmp(str, subStr, strlen(subStr) - 1) == 0)
+    {
+        return 1;
+    }
+    return verStr(str + 1, subStr);
+}
+
+int main(int argc, char const *argv[])
+{
+    char str[50], subStr[50];
+
+    printf("Digite a frase completa: ");
+    fgets(str, 49, stdin);
+
+    printf("Digite a SubFrase: ");
+    fgets(subStr, 49, stdin);
+
+    if (verStr(str, subStr))
+    {
+        printf("\n%s Eh uma SubString de '%s\n", subStr, str);
+    }
+    else
+    {
+        printf("\n%s Nao eh uma SubString de %s\n", subStr, str);
+    }
+
+    return 0;
+}
